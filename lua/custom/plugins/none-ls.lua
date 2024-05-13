cd344e08082bc4f2e83fdfa8a56d441849bda9ca
Alexandr Pleskot <alexandrpleskot@gmail.com>
2024-05-13 08:15:10 +0200

    update deps

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,16 +1,16 @@
 return {
   'nvimtools/none-ls.nvim',
   config = function()
-    local null_ls = require('null-ls')
-    null_ls.setup({
+    local null_ls = require 'null-ls'
+    null_ls.setup {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.zigfmt,
-        null_ls.builtins.diagnostics.typos,
-        null_ls.builtins.diagnostics.eslint_d,
-      }
-    })
-  end
+        -- null_ls.builtins.formatting.rustfmt,
+        -- null_ls.builtins.formatting.zigfmt,
+        -- null_ls.builtins.diagnostics.typos,
+        -- null_ls.builtins.diagnostics.eslint_d,
+      },
+    }
+  end,
 }
