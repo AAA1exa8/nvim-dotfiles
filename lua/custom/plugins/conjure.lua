4a8d7919e395b6d37d909e85c807496d76af86b7
Alexandr Pleskot <alexandrpleskot@gmail.com>
2024-12-11 12:57:59 +0100

    turn on lazy loading

--- lua/custom/plugins/conjure.lua
+++ lua/custom/plugins/conjure.lua
@@ -5,10 +5,13 @@ return {
   {
     'Olical/conjure',
     ft = { 'clojure', 'fennel', 'racket' },
+    lazy = true,
     init = function()
       vim.g['conjure#mapping#doc_word'] = { 'gk' }
     end,
     dependencies = { 'PaterJason/cmp-conjure' },
   },
-  { 'PaterJason/cmp-conjure' },
+  { 
+    'PaterJason/cmp-conjure', lazy = true
+  },
 }
