aebb68a4f4eb0adc2e856ff26945371a8f3fbc58
Alexandr Pleskot <alexandrpleskot@gmail.com>
2024-12-26 13:19:25 +0100

    conform

---
+++ lua/custom/plugins/conform.lua
@@ -0,0 +1,43 @@
+return {
+  'stevearc/conform.nvim',
+  ---@module "conform"
+  ---@type conform.setupOpts
+  opts = {
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_format = 'fallback',
+    },
+    formatters_by_ft = {
+      c = { 'clang_format' },
+    },
+  },
+  init = function()
+    require('conform').setup {
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = 'fallback' }
+      end,
+    }
+
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
+  end,
+}
