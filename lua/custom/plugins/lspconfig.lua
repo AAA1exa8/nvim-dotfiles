b607097926a5faa0d1bf6a4b59ae1843bd52e465
Alexandr Pleskot <alexandrpleskot@gmail.com>
2025-05-12 15:20:30 +0200

    rustecean

--- lua/custom/plugins/lspconfig.lua
+++ lua/custom/plugins/lspconfig.lua
@@ -3,6 +3,7 @@ return {
   'neovim/nvim-lspconfig',
   dependencies = {
 
+    'mrcjkb/rustaceanvim',
     { 'mason-org/mason.nvim', opts = {} },
     'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
